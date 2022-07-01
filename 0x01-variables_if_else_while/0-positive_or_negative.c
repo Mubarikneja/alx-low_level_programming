e25e19b0763d68b5f75089b258070e88535c52a9
Mubarikneja <mubarikneja@gmail.com>
2022-07-01 07:44:16 -0700

    all

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,20 +5,16 @@
 /* main method to check the number is +,- or 0 */
 int main(void) /* main function to compare the rundom number */
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* n val comparision  */
-	if (n > 0)
-		printf("%i is positive\n", n);
-	else if (n < 0)
-		printf("%i is negative\n", n);
-	else
-		printf("%i is zero\n", n);
-
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* n val comparision  */
+if (n > 0)
+printf("%i is positive\n", n);
+else if (n < 0)
+printf("%i is negative\n", n);
+else
+printf("%i is zero\n", n);
+return (0);
 }
-
-
-
