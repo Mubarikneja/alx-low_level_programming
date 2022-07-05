599523a67548b390b2a4160ef75d6e7d1c53c7ca
Mubarikneja <mubarikneja@gmail.com>
2022-07-05 03:50:06 -0700

    add uip

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * * main - check the code
+ * * add - add two integer
  * *
  * * Return: Always 0.
- * */
-
+ */
 int add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
