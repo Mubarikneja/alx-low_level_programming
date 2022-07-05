68cc3aff04938fae05d85c1b5b04d74e3ad5ca39
Mubarikneja <mubarikneja@gmail.com>
2022-07-05 00:16:09 -0700

    code update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- *  * main - check the code.
- *   *
- *    * Return: Always 0.
- *     */
- 
+ * * main - check the code.
+ * *
+ * * Return: Always 0.
+ * */
+
 int _islower(int c)
 {
 
-		if (c >= 97 && c <= 122)
-					return (1);
-			else
-						return (0);
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
 }
