8a58715083be68e02b6d724d17f349dc24867375
Mubarikneja <mubarikneja@gmail.com>
2022-07-07 03:39:48 -0700

    change

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,6 +4,7 @@
 /**
  *   _isdigit - Check if paramter c represents an digit.
  *     @c: Int representing an ascii character
+ *
  *       Return: Return 1 if it is a digit otherwise return 0.
  */
 int _isdigit(int c)
