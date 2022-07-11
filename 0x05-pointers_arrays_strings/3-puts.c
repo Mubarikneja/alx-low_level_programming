4c72a314fc71c25918b79975000127a278d64107
Mubarikneja <mubarikneja@gmail.com>
2022-07-11 04:04:45 -0700

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,8 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * * _puts - prints a string, followed by a new line,
  * * @str: pointer to the string to print
+ * *
  * * Return: void
  */
 
@@ -9,11 +10,10 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i])
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(*str);
+		str++;
 	}
 	_putchar('\n');
 }
-
