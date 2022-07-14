532b497b8d9c14634140a387a40321f10ab8648d
Mubarikneja <mubarikneja@gmail.com>
2022-07-14 09:28:03 -0700

    jfdgfd

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _strncat - two words
- * @dest : pointer to char param
- * @src : pointer to char param
+ * * _strncat - two words
+ * @ *dest : pointer to char param
+ * @ *src : pointer to char param
  * @n : int parameter
  * 
  * Return: *dest
