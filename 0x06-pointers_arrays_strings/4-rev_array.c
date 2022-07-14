e689b92d893c9ee853442dfdc0300a26f7190d66
Mubarikneja <mubarikneja@gmail.com>
2022-07-14 08:21:53 -0700

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * * reverse_array - reverse
- * * @a : pointer to int params
- * * @n : pointer to int params
- * *
- * * Return: no
- * */
+ * reverse_array - reverse
+ *  @a : pointer to int params
+ *  @n : pointer to int params
+ * 
+ *  Return: no
+ */
 
 void reverse_array(int *a, int n)
 {
 	int i;
-
 	int j;
-
 	int tmp;
 
 	i = 0;
