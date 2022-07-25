54c66700f8ab29b9572503fb1abe72ab0c27b399
Mubarikneja <mubarikneja@gmail.com>
2022-07-25 00:01:50 -0700

    1- all updated

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,12 +18,14 @@ char *_strdup(char *str)
 		return (NULL);
 
 	for (; str[size] != '\0'; size++)
-
-	/*+1 on the size puts the end of string character*/
-	m = malloc(size * sizeof(*str) + 1);
+			
+		/*+1 on the size puts the end of string character*/
+		m = malloc(size * sizeof(*str) + 1);
 
 	if (m == 0)
+	{
 		return (NULL);
+	}
 	else
 	{
 		for (; i < size; i++)
