7daf08818a312c2ee8fc8209e05a99aeab3dafd3
Mubarikneja <mubarikneja@gmail.com>
2022-07-25 21:36:02 -0700

    100 modif

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,47 +1,46 @@
-#include <stdlib.h>
+#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * * _strlen - find length of a string
- * * @s: string
- * * Return: int
+ * * argstostr - concatenates all the arguments of your program
+ * *@ac: number of arguments
+ * *@av: arguments
+ * * Return: a pointer to a new string
  */
-
-int _strlen(char *s)
-{
-	int size = 0;
-	for (; s[size] != '\0'; size++)
-		return (size);
-}
-
-/**
- * * *argstostr - description
- * * @ac: int
- * * @av: arguments
- * * Return: string
- */
-
 char *argstostr(int ac, char **av)
 {
-	int i = 0, nc = 0, j = 0, cmpt = 0;
-	char *s;
+	int i;
+	int j;
+	char *p = NULL;
+	int k;
+	int ext;
 
+	k = 0;
+	ext = 0;
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (; i < ac; i++, nc++)
-		nc += _strlen(av[i]);
-	s = malloc(sizeof(char) * nc + 1);
-	if (s == 0)
-		return (NULL);
-
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
-			s[cmpt] = av[i][j];
-
-		s[cmpt] = '\n';
-		cmpt++;
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			ext++;
+		}
 	}
-	s[cmpt] = '\0';
-	return (s);
+
+	p = (char *)malloc(ext + ac + 1 * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			p[k] = av[i][j];
+			k++;
+		}
+		p[k] = '\n';
+		k++;
+	}
+	p[k] = '\0';
+	return (p);
 }
