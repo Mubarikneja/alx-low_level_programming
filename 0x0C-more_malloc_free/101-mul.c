2da0fbddb5217bffb731d6ff523eb18bfa0b13c4
Mubarikneja <mubarikneja@gmail.com>
2022-07-28 05:57:07 -0700

    all

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,9 +11,11 @@ int main(int argc, char *argv[])
 {
 unsigned long mul;
 int i, j;
+
 if (argc != 3)
 { printf("Error\n");
 exit(98); }
+
 for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j] != '\0'; j++)
@@ -26,5 +28,7 @@ exit(98); }
 }
 mul = atol(argv[1]) *  atol(argv[2]);
 printf("%lu\n", mul);
+
 return (0);
 }
+
