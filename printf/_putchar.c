5b6f02ccc5dca322ae0114bf10750a3d9c58207f
Mubarik Neja <mubarikneja@gmail.com>
2022-08-08 17:43:11 +0300

    put char

--- printf/_putchar.c
+++ printf/_putchar.c
@@ -1,13 +1,13 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* _putchar - writes 
+* @c: The print
+*
+* Return: On success 1.
+* On error, -1 is returned
+*/
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
