6af7a0e6154fcc58b6133f6db713e573922de292
Mubarik Neja <mubarikneja@gmail.com>
2022-08-17 07:07:46 +0300

    change on 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,16 +15,13 @@ listint_t *temp, *temp2;
 temp = malloc(sizeof(listint_t));
 if (temp == NULL)
 return (NULL);
-
 temp->n = n;
 temp->next = NULL;
-
 if (*head == NULL)
 {
 *head = temp;
 return (temp);
 }
-
 temp2 = *head;
 while (temp2->next)
 temp2 = temp2->next;
