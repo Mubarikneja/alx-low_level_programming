4c7e9b1d1584e5a4316ec6114591b399cc87526b
Mubarik Neja <mubarikneja@gmail.com>
2022-08-17 07:17:47 +0300

    5 modif

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,21 +1,23 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
-* free_listint2 - Frees a list.
-* @head: Address 
+* free_listint2 - frees 
+* Description: at the end
+* So, as we know the tail 
+* @head: head of linked list
 */
 
 void free_listint2(listint_t **head)
 {
-listint_t *temp;
+listint_t *current;
 
 if (head == NULL)
 return;
+
 while (*head != NULL)
 {
-temp = (*head)->next;
-free(*head);
-*head = temp;
+current = *head;
+*head = (*head)->next;
+free(current);
 }
 }
