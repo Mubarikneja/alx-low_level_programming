207980d42629958e4b1e6a2b127762b853089011
Mubarik Neja <mubarikneja@gmail.com>
2022-08-18 13:49:48 +0300

    5 and advanced

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,19 +4,19 @@
 * flip_bits - number of bit
 * @n: parameter
 * @m: destiny
-* Return: n
+* Return: nflips
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-unsigned int k, n = 0;
+unsigned int i, nflips = 0;
 unsigned long int j = sizeof(unsigned long int) * 8;
 
-for (k = 0; k < j; k++)
+for (i = 0; i < j; i++)
 {
 if ((m & 1) != (n & 1))
-n += 1;
+nflips += 1;
 n = n >> 1;
 m = m >> 1;
 }
-return (n);
+return (nflips);
 }
