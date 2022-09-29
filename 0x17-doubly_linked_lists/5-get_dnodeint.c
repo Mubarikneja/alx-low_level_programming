de3e8a78ce3ac97396bf8b32fbbf8a27281aaa67
Mubarik Neja <mubarikneja@gmail.com>
2022-09-29 18:34:46 +0300

    5 is modif

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,28 +1,20 @@
-#include "list.h"
+#include "lists.h"
 
 /**
- * get_dnodeint_at_index - returns the nth node
- * @head: head of linked list
- * @index: node index to return
- * Return: node
+ * get_dnodeint_at_index - Locates a node in the list.
+ * @head: The head 
+ * @index: The index of the node to locate.
+ *
+ * Return:  NULL or address of the located node.
  */
-
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *current;
-
-	if (head == NULL)
-		return (0);
-
-	current = head;
-
-	while (index != 0)
+	for (; index != 0; index--)
 	{
-		current = current->next;
-		index--;
-		if (current == NULL)
-			return (0); /*Out of range*/
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
 	}
 
-	return (current);
+	return (head);
 }
