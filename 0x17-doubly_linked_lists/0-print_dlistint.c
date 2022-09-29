a4eb262156d5bfa4f9d9439bb91bdaae79816474
Mubarik Neja <mubarikneja@gmail.com>
2022-09-29 13:37:30 +0300

    updated

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "list.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - print element
+ * @h: linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nbr_nodes = 0;
+	const dlistint_t *now = h;
+
+	while (now != NULL)
+	{
+		printf("%i\n", now->n);
+		now = now->next;
+		nbr_nodes++;
+	}
+
+	return (nbr_nodes);
+}
